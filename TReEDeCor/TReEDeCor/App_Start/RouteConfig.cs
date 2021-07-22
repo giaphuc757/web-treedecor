c9637734cd1fef013fb0e84639ae10375def4b82
phuc hoang <giaphuc757@gmail.com>
2021-07-22 20:45:08 +0700

    Initial commit

---
+++ TReEDeCor/TReEDeCor/App_Start/RouteConfig.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TReEDeCor
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "PhroDuct",
+                url: "san-pham",
+                defaults: new { controller = "PhroDuct", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "trangchu",
+                url: "",
+                defaults: new { controller = "User", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "TinTuc",
+                url: "tin-tuc",
+                defaults: new { controller = "TinTuc", action = "TinTucNew", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "SuKien",
+                url: "su-kien",
+                defaults: new { controller = "SuKien", action = "SuKienNew", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+              name: "BaoHanh-HauMai",
+              url: "bao-hanh-hau-mai",
+              defaults: new { controller = "BHHM", action = "BaoHanh_HauMai", id = UrlParameter.Optional }
+          );
+            routes.MapRoute(
+              name: "DangNhap",
+              url: "dang-nhap",
+              defaults: new { controller = "Dangnhap", action = "Nguoidung", id = UrlParameter.Optional }
+          );
+            routes.MapRoute(
+                name: "Lien He",
+                url: "lien-he",
+                defaults: new { controller = "Contact", action = "Contact", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TReEDeCor/TReEDeCor/Controllers/AdminController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TReEDeCor.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace TReEDeCor.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        DatabaseDataContext db = new DatabaseDataContext();
+        public ActionResult Index()
+        {
+            if (Session["TKAdmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            
+            return View();
+        }
+        [HttpGet]
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var tendn = collection["username"];
+            var matkhau = collection["pass"];
+
+            
+                Admin ad = db.Admins.SingleOrDefault(n => n.UserAdmin == tendn && n.PassAdmin == matkhau);
+                if (ad != null)
+                {
+
+                    Session["TKAdmin"] = ad;
+                    return RedirectToAction("Index", "Admin");
+
+                }
+                else
+                    ViewBag.ThongBao = "Tên Đăng Nhập Hoặc Tài Khoản Không Đúng";
+            
+            return View();
+        }
+
+
+        ///////////////////////////////////
+        public ActionResult Product()
+        {
+            return View(db.SANPHAMs.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            if (Session["TKAdmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            else
+            {
+                var sanpham = from SANPHAM in db.SANPHAMs where SANPHAM.MaSP == id select SANPHAM;
+                return View(sanpham.SingleOrDefault());
+            }
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            if (Session["TKAdmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            else
+                return View();
+        }
+        [HttpPost]
+        public ActionResult Create(SANPHAM sanpham)
+        {
+            if (Session["TKAdmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            else
+            {
+                db.SANPHAMs.InsertOnSubmit(sanpham);
+                db.SubmitChanges();
+
+                return View("Index","Admin");
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            if (Session["TKAdmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            else
+            {
+                var sanpham = from SANPHAM in db.SANPHAMs where SANPHAM.MaSP == id select SANPHAM;
+                return View(sanpham.SingleOrDefault());
+            }
+        }
+        [HttpPost, ActionName("Edit")]
+        public ActionResult capnhat(int id)
+        {
+            SANPHAM sanpham = db.SANPHAMs.Where(n => n.MaSP == id).SingleOrDefault();
+            UpdateModel(sanpham);
+            db.SubmitChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            if (Session["TKAdmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            else
+            {
+                var sanpham = from SANPHAM in db.SANPHAMs where SANPHAM.MaSP == id select SANPHAM;
+                return View(sanpham.SingleOrDefault());
+            }
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult xoasp(int id)
+        {
+            SANPHAM sanpham = db.SANPHAMs.Where(n => n.MaSP == id).SingleOrDefault();
+            db.SANPHAMs.DeleteOnSubmit(sanpham);
+            db.SubmitChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+    }
+}
